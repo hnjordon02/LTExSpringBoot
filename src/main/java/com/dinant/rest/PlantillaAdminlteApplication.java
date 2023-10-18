8e434920d00676509e12443b8ba2964c2ecb10bc
Jose Angel Ordoñez Matamoros <Jose.Ordonez@Dinant.com>
2023-10-18 17:05:59 -0600

    Plantila

---
+++ src/main/java/com/dinant/rest/PlantillaAdminlteApplication.java
@@ -0,0 +1,13 @@
+package com.dinant.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantillaAdminlteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantillaAdminlteApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/static/vendor/js/MainFuctions.js
@@ -0,0 +1,54 @@
+var tableExample;
+document.addEventListener('DOMContentLoaded',function(e){
+	
+	tableExample = $('#tableExample').dataTable({
+		 "aProcessing":true,
+        "aServerSide":true,
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json"
+        },
+        "ajax":{
+            "url": " "+base_url+"/Categorias/getCategorias",
+            "dataSrc":""
+        },
+        "columns":[
+            {"data":"id"}, 
+            {"data":"descripcion"},
+            {"data":"status"},
+            {"data":"options"}
+        ],
+        'dom': "<'row'<'col-sm-4'l><'col-sm-4 text-center'B><'col-sm-4'f>>" +
+                "<'row'<'col-sm-12'tr>>" +
+                "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+        'buttons': [
+            {
+                "extend": "copyHtml5",
+                "text": "<i class='far fa-copy'></i> Copiar",
+                "titleAttr":"Copiar",
+                "className": "btn btn-secondary"
+            },{
+                "extend": "excelHtml5",
+                "text": "<i class='fas fa-file-excel'></i> Excel",
+                "titleAttr":"Exportar a Excel",
+                "className": "btn btn-success"
+            },{
+                "extend": "pdfHtml5",
+                "text": "<i class='fas fa-file-pdf'></i> PDF",
+                "titleAttr":"Exportar a PDF",
+                "className": "btn btn-danger"
+            },{
+                "extend": "csvHtml5",
+                "text": "<i class='fas fa-file-csv'></i> CSV",
+                "titleAttr":"Exportar a CSV",
+                "className": "btn btn-info"
+            }
+        ],
+        "resonsieve":"true",
+        "bDestroy": true,
+        "iDisplayLength": 10,
+        "order":[[0,"desc"]]  
+    });
+		
+		
+});
+	
